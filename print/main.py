dcddbf8853ac4f927e6585bbc99f60b8fab49db0
FabiusMusk <132197839+FabiusMusk@users.noreply.github.com>
2023-05-03 07:54:56 +0200

    Add files via upload

---
+++ print/main.py
@@ -0,0 +1,12 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print('Hello, world')
+print('Hi, my name Fabius')
+print('Fabulouse')
+print('Fantastic')
+print('Awesome to learn some code with python')
+print(16)
+print(1,2,3,4,5)
